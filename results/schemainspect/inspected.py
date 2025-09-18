f7f10decd5c21c9d67299e203c88e9de7979587c
Patrick Griffin <pgriffin@techflow.com>
2025-09-18 16:01:05 -0500

    Adding .rel_options property to equality check for InspectedSelectable instances

--- results/schemainspect/inspected.py
+++ results/schemainspect/inspected.py
@@ -328,5 +328,6 @@ class InspectedSelectable(Inspected):
             self.partition_def == other.partition_def,
             self.rowsecurity == other.rowsecurity,
             self.persistence == other.persistence,
+            self.rel_options == other.rel_options,
         )
         return all(equalities)
